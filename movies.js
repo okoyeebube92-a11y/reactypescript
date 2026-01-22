8a0cabe97a8198712986f4def4bbc5b59e9f1067
Okoye Biggie <okoyeebube92@gmail.com>
2026-01-22 12:59:43 +0100

    Add files via upload

---
+++ movies.js
@@ -0,0 +1,20 @@
+const moviesData = [
+  {
+    id: 1,
+    title: "Inception",
+    description: "A mind-bending thriller about dreams within dreams.",
+    posterURL: "https://m.media-amazon.com/images/I/51v5ZpFyaFL._AC_.jpg",
+    rating: 5,
+    trailer: "https://www.youtube.com/embed/YoHD9XEInc0"
+  },
+  {
+    id: 2,
+    title: "Interstellar",
+    description: "Exploration of space, time, and human survival.",
+    posterURL: "https://m.media-amazon.com/images/I/71n58lZ1xOL._AC_SY679_.jpg",
+    rating: 4,
+    trailer: "https://www.youtube.com/embed/zSWdZVtXT7E"
+  }
+];
+
+export default moviesData;
